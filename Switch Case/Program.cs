c42ab730e1d681a84d87e1c1a70b5475b0b49267
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-23 18:06:36 +0300

    Switch Case odevi yapildi

---
+++ Switch Case/Program.cs
@@ -0,0 +1,46 @@
+﻿int month= DateTime.Now.Month;
+//Expression
+switch (month) 
+{
+  case 1:
+  Console.WriteLine("Ocak ayindasiniz");
+    break;
+  case 2:
+  Console.WriteLine("Subat ayindasiniz");
+    break;
+  case 4:
+  Console.WriteLine("Nisan ayindasiniz");
+    break;
+  case 3:
+  Console.WriteLine("Mart ayindasiniz");
+    break;
+    default:
+    Console.WriteLine("Yanlis veri girisi");
+    break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+    Console.WriteLine("Kis ayindasiniz!");
+       break;
+    case 3:
+    case 4:
+    case 5:
+    Console.WriteLine("ilkbahar ayindasiniz!");
+       break;
+    case 6:
+    case 7:
+    case 8:
+    Console.WriteLine("Yaz ayindasiniz!");
+       break;
+    case 9:
+    case 10:
+    case 11:
+    Console.WriteLine("Sonbahar ayindasiniz!");
+       break;
+    default:
+    break;
+}
